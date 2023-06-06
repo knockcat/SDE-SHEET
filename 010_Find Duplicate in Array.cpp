a3f45f36fec45dfd574e038d3f2415c691903b19
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-06-06 06:59:33 +0530

    Find Duplicate in Array

---
+++ 010_Find Duplicate in Array.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+int findDuplicate(vector<int> &arr, int n){
+	// Write your code here.
+
+	int slow = arr[0];
+	int fast = arr[0];
+
+	do{
+		slow = arr[slow];
+		fast = arr[arr[fast]];
+	}while(slow != fast);
+
+	fast = arr[0];
+
+	while(slow != fast)
+	{
+		slow = arr[slow];
+		fast = arr[fast];
+	}
+
+	return slow;
+
+}
